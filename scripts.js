417a170ff9b56a6a112547ccc4b61a508d199332
Pedro Passos <pedro.passos081@gmail.com>
2025-05-28 13:55:20 -0300

    Adicionando 2 funcionalidades uma de enviar arquivo e outra de conseguir colocar novas tags

--- scripts.js
+++ scripts.js
@@ -4,3 +4,49 @@ const inputUpload = document.getElementById("image-upload");
 uploadBtn.addEventListener("click", () => {
   inputUpload.click();
 });
+
+function lerConteudoArquivo(arquivo) {
+  return new Promise((resolve, reject) => {
+    const leitor = new FileReader();
+    leitor.onload = () => {
+      resolve({ url: leitor.result, nome: arquivo.name });
+    };
+    leitor.onerror = () => {
+      reject(`Erro ao ler o arquivo: ${arquivo.name}`);
+    };
+    leitor.readAsDataURL(arquivo);
+  });
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) => {
+  const arquivo = evento.target.files[0];
+  if (arquivo) {
+    try {
+      const conteudoArquivo = await lerConteudoArquivo(arquivo);
+      imagemPrincipal.src = conteudoArquivo.url;
+      nomeImagem.textContent = conteudoArquivo.nome;
+    } catch (erro) {
+      console.error(erro);
+      alert("Erro ao carregar a imagem. Por favor, tente novamente.");
+    }
+  }
+});
+
+const inputTags = document.getElementById("input-tags");
+const listaTags = document.getElementById("lista-tags");
+
+inputTags.addEventListener("keypress", (evento) => {
+  if (evento.key === "Enter") {
+    evento.preventDefault();
+    const tagTexto = inputTags.value.trim();
+    if (tagTexto !== "") {
+      const newTag = document.createElement("li");
+      newTag.innerHTML = `<p>${tagTexto}</p> <img src="img/close-black.svg" class="remove-tag"`;
+      listaTags.appendChild(newTag);
+      inputTags.value = ""; // Limpa o campo de entrada
+    }
+  }
+});
